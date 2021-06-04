79b67b0570f756f236fd9f2d10793fc49a680bca
richanghmc <sg626.richardchang@gmail.com>
2021-06-04 10:40:54 -0700

    Basic dimension of labware

--- CADFileAdapter.py
+++ CADFileAdapter.py
@@ -1,4 +1,21 @@
 import openpyscad as ops
 
-c1 = ops.Cube([10, 20, 10])
-c2 = ops.Cube([20, 10, 10])
+# Opentron modules dimensions
+# Thermocycler module - 316 x 172 x 154mm (l x w x h)
+# Temperature module - 194 x 90 x 128mm
+# Magnetic module - 128mm x 86mm x 90mm
+#
+# Sample labware - 130mm x 130mm x 50mm
+#
+
+Thermocycler_mod = ops.Cube([31.6, 17.2, 15.4]).color("Red").translate([0,0,0])
+Temperature_mod = ops.Cube([19.4, 9, 12.8]).color("Blue").translate([0,50,0])
+Magnetic_mod = ops.Cube([12.8, 8.6, 9]).color("Green").translate([50,0,0])
+Sample_labware = ops.Cube([13, 13, 5]).color("Yellow").translate([50,50,0])
+
+Thermocycler_mod.write("dimension.scad")
+Temperature_mod.write("dimension.scad")
+Magnetic_mod.write("dimension.scad")
+Sample_labware.write("dimension.scad")
+
+(Temperature_mod + Thermocycler_mod + Magnetic_mod + Sample_labware).write("dimension.scad")
---
+++ dimension.scad
@@ -0,0 +1,22 @@
+union(){
+    translate(v=[0, 50, 0]){
+        color("Blue"){
+            cube(size=[19.4, 9, 12.8]);
+        };
+    };
+    translate(v=[0, 0, 0]){
+        color("Red"){
+            cube(size=[31.6, 17.2, 15.4]);
+        };
+    };
+    translate(v=[50, 0, 0]){
+        color("Green"){
+            cube(size=[12.8, 8.6, 9]);
+        };
+    };
+    translate(v=[50, 50, 0]){
+        color("Yellow"){
+            cube(size=[13, 13, 5]);
+        };
+    };
+};
